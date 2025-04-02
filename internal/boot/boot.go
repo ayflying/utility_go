69e72283dcb38684825d3483ea47c9e90139fd98
ayflying <ay@7cuu.com>
2025-04-02 11:51:25 +0800

    更新

--- internal/boot/boot.go
+++ internal/boot/boot.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	v1 "github.com/ayflying/utility_go/api/system/v1"
 	"github.com/ayflying/utility_go/service"
-	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/gtimer"
-	"github.com/prometheus/client_golang/prometheus"
 	"time"
 )
 
