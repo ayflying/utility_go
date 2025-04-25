e9962f1b74040886fbaf25d5797e87bbb110c483
ayflying <ay@7cuu.com>
2025-04-25 11:28:18 +0800

    修改act数据取出的时候进行泛型复制而不是转格式

--- internal/logic/gameAct/gameAct.go
+++ internal/logic/gameAct/gameAct.go
@@ -9,6 +9,7 @@ import (
 	service2 "github.com/ayflying/utility_go/service"
 	"github.com/ayflying/utility_go/tools"
 	"github.com/gogf/gf/v2/container/gset"
+	"github.com/gogf/gf/v2/container/gvar"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/gtime"
@@ -63,8 +64,11 @@ func (s *sGameAct) Info(uid int64, actId int) (data *g.Var, err error) {
 		Uid:   uid,
 		ActId: actId,
 	}).Fields("action").OrderDesc("updated_at").Value()
-	getDb.Scan(&data)
-
+	//getDb.Scan(&data)
+	if getDb == nil || getDb.IsEmpty() {
+		return
+	}
+	data = gvar.New(getDb)
 	if data == nil || data.IsEmpty() {
 		return
 	}
