b10eaf5133ece03c74814a1a4d46a133b544e454
yaodeshun <yaodeshun@adesk.com>
2025-10-11 16:02:14 +0800

    新增过滤字符

--- package/gamelog/sdk.go
+++ package/gamelog/sdk.go
@@ -68,7 +68,14 @@ var (
 
 )
 
-var safePropertyRE = regexp.MustCompile(`["'\\\/]`)
+var safePropertyRE = regexp.MustCompile(
+	`["'\\\/]` +
+		`|\\U[0-9a-fA-F]{8}` + // Unicode 8位转义
+		`|\\u[0-9a-fA-F]{4}` + // Unicode 4位转义
+		`|\\[tnrfbv\\]` + // 转义字面量
+		`|[\x00-\x1F\x7F-\x9F]` + // ASCII + C1 控制字符
+		`|[\u200B-\u200D\uFEFF]`, // 零宽字符
+)
 
 func safeProperty(property map[string]any) {
 	for k, v := range property {
