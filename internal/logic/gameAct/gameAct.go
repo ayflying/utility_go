d82b12ddaf952c8522beba22bebb2a1f5535d959
liaoyulong <liaoyulong@adesk.com>
2025-11-19 14:56:12 +0800

    Revert "解决后续data解析到空map为nil的问题"
    This reverts commit f3e1ad74b51d2a71c6819017aced993559427f69.

--- internal/logic/gameAct/gameAct.go
+++ internal/logic/gameAct/gameAct.go
@@ -15,7 +15,6 @@ import (
 	service2 "github.com/ayflying/utility_go/service"
 	"github.com/ayflying/utility_go/tools"
 	"github.com/gogf/gf/v2/container/gset"
-	"github.com/gogf/gf/v2/container/gvar"
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
@@ -69,8 +68,8 @@ func (s *sGameAct) Info(uid int64, actId int) (data *g.Var, err error) {
 		Uid:   uid,
 		ActId: actId,
 	}).Fields("action").OrderDesc("updated_at").Value()
-	// getDb.Scan(&data)
-	data = gvar.New(getDb)
+	getDb.Scan(&data)
+
 	if data == nil || data.IsEmpty() {
 		return
 	}
